6cfde39f6229c1aa35d760f1ab12427b96787f77
VozhegovAM <Alex3ind@gmail.com>
2024-03-29 13:33:24 +0300

    third changes

--- model_search/architecture/architecture_utils.py
+++ model_search/architecture/architecture_utils.py
@@ -2,7 +2,7 @@
 #
 # Licensed under the CHANGES2
 # you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# You may obtain a copy of the License atCHANGES3
 #
 #      http://www.apache.org/licenses/LICENSE-2.0
 #
