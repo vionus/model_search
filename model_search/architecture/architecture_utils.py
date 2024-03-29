7ad1a69e94157d15fcf7ef56273bd6232843a477
VozhegovAM <Alex3ind@gmail.com>
2024-03-29 13:33:00 +0300

    second changes

--- model_search/architecture/architecture_utils.py
+++ model_search/architecture/architecture_utils.py
@@ -1,6 +1,6 @@
 # Copyright 2020 Google LLC CHANGES1
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
+# Licensed under the CHANGES2
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
